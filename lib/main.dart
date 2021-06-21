d0bed11924f9c81cc388e79f48196985886a0529
akramtrabelsi <akramtrabelsikaroui@gmail.com>
2021-06-21 17:01:30 +0100

    initial project setup7

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GeneralShop());
+}
+
+class GeneralShop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'General Shop',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('General Shop'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('General Shop'),
+        ),
+      ),
+    );
+  }
+}
